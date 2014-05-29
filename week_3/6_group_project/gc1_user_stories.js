3f7ca8c6f196cdba6e7c9f5c81eeb8bf84518843
George Zeng <george.x.zeng@gmail.com>
2014-05-29 01:08:27 -0400

    modified tests

--- week_3/6_group_project/gc1_user_stories.js
+++ week_3/6_group_project/gc1_user_stories.js
@@ -15,6 +15,6 @@
 
 // Test 3:
 // For the third test, we want to find the number that is directly in the middle
-// of a list, for a list with an odd amount of numbers.  If the list has an odd amount of numbers,
+// of a list, for a list with an odd amount of numbers.  If the list has an even amount of numbers,
 // we want to find the average of the two middlemost numbers.  Return this value after these steps are done.
 
